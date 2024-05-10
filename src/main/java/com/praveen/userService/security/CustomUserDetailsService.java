530613ad4857337922208c465036195e353f5004
praveenselvaraj <praveen.selvaraj997@gmail.com>
2024-05-10 12:04:37 +0530

    added basic spring oauth2 server

---
+++ src/main/java/com/praveen/userService/security/CustomUserDetailsService.java
@@ -0,0 +1,44 @@
+package com.praveen.userService.security;
+
+import com.praveen.userService.models.User;
+import com.praveen.userService.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    /**
+     * Locates the user based on the email. In the actual implementation, the search
+     * may possibly be case sensitive, or case insensitive depending on how the
+     * implementation instance is configured. In this case, the <code>UserDetails</code>
+     * object that comes back may have a username that is of a different case than what
+     * was actually requested..
+     *
+     * @param email the username identifying the user whose data is required.
+     * @return a fully populated user record (never <code>null</code>)
+     * @throws UsernameNotFoundException if the user could not be found or the user has no
+     *                                   GrantedAuthority
+     */
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<User> optionalUser = userRepository.findUserByEmail(email);
+
+        if(optionalUser.isEmpty()){
+            throw new UsernameNotFoundException("User with "  + email + " not found");
+        }
+
+        User user = optionalUser.get();
+
+        return new CustomUserDetails(user);
+    }
+}
